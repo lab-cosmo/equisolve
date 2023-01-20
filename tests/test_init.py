83cb654f84078c08ce6067a3d496dd10386b9da4
Philip Loche <ploche@physik.fu-berlin.de>
2023-01-20 11:36:25 +0100

    Update and add copyright information

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,10 @@
 # -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
 #
-# Copyright (c) 2022 Authors and contributors
+# Copyright (c) 2023 Authors and contributors
 # (see the AUTHORS.rst file for the full list of names)
 #
-# Released under the GNU Public Licence, v3 or any higher version
-# SPDX-License-Identifier: GPL-3.0-or-later
+# Released under the BSD 3-Clause "New" or "Revised" License
+# SPDX-License-Identifier: BSD-3-Clause
 
 import equisolve
 
--- tests/utils/test_convert.py
+++ tests/utils/test_convert.py
@@ -1,3 +1,11 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+#
+# Copyright (c) 2023 Authors and contributors
+# (see the AUTHORS.rst file for the full list of names)
+#
+# Released under the BSD 3-Clause "New" or "Revised" License
+# SPDX-License-Identifier: BSD-3-Clause
+
 import ase
 import numpy as np
 import pytest
