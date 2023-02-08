f98c981d4c86288b37b327d96efadb474ccf6a88
Davide Tisi <47503434+DavideTisi@users.noreply.github.com>
2023-02-08 20:23:26 +0100

    modified predict function to use the dot between tensormap now it is only one line of code + check - constructed the coef_tensor which is the tensorMap of the coeff - i left the old implementation commented in case you do not like it and to compare so far both coef_ and coef_tensor exist so the check are still on coeff_ . maybe we want to change that.

--- src/equisolve/numpy/models/linear_model.py
+++ src/equisolve/numpy/models/linear_model.py
@@ -10,6 +10,7 @@ from typing import List, Optional, Union
 
 import numpy as np
 from equistore import Labels, TensorBlock, TensorMap
+from equistore.operations import dot
 from equistore.operations._utils import _check_blocks, _check_maps
 
 from ...utils.metrics import rmse
@@ -157,6 +158,7 @@ class Ridge:
         self._validate_params(X, sample_weight)
 
         coef = []
+        coef_tensor = []
         for key, X_block in X:
             y_block = y.block(key)
             alpha_block = self.alpha.block(key)
@@ -183,10 +185,18 @@ class Ridge:
 
             w = self._numpy_lstsq_solver(X_arr, y_arr, sw_arr, alpha_arr, self.rcond)
             coef.append(w.reshape(-1, 1))
+            coef_block = TensorBlock(
+                values=w.reshape(-1, 1).T,
+                samples=y_block.properties,
+                components=[],
+                properties=X_block.properties,
+            )
+            coef_tensor.append(coef_block)
 
         # Convert alpha to a dictionary to be used in external models.
         self.alpha = tensor_map_to_dict(self.alpha)
         self.coef_ = coef
+        self.coef_tensor = TensorMap(X.keys, coef_tensor)
 
         return self
 
@@ -206,41 +216,41 @@ class Ridge:
                 f"Number of blocks in X ({len(X.blocks())}) does not agree "
                 f"with the number of fitted weights ({len(self.coef_)})."
             )
-        self.alpha = dict_to_tensor_map(self.alpha)
+        # self.alpha = dict_to_tensor_map(self.alpha)
 
-        blocks = []
-        for i_block, X_block in enumerate(X.blocks()):
-            w = self.coef_[i_block]
-            n_samples = len(X_block.samples)
+        # blocks = []
+        # for i_block, X_block in enumerate(X.blocks()):
+        #     w = self.coef_[i_block]
+        #     n_samples = len(X_block.samples)
 
-            block = TensorBlock(
-                values=np.zeros([n_samples, 1]),
-                samples=Labels(["structure"], np.arange(n_samples).reshape(-1, 1)),
-                components=[],
-                properties=Labels(["property"], np.array([(0,)])),
-            )
+        #     block = TensorBlock(
+        #         values=np.zeros([n_samples, 1]),
+        #         samples=Labels(["structure"], np.arange(n_samples).reshape(-1, 1)),
+        #         components=[],
+        #         properties=Labels(["property"], np.array([(0,)])),
+        #     )
 
-            for parameter in self.parameter_keys:
-                if parameter == "values":
-                    block.values[:] = X_block.values @ w
-                else:
-                    X_gradient = X_block.gradient(parameter)
+        #     for parameter in self.parameter_keys:
+        #         if parameter == "values":
+        #             block.values[:] = X_block.values @ w
+        #         else:
+        #             X_gradient = X_block.gradient(parameter)
 
-                    data_flat = X_gradient.data.reshape(
-                        np.prod(X_gradient.data.shape[:-1]), X_gradient.data.shape[-1]
-                    )
-                    data_flat = data_flat @ w
+        #             data_flat = X_gradient.data.reshape(
+        #                 np.prod(X_gradient.data.shape[:-1]), X_gradient.data.shape[-1]
+        #             )
+        #             data_flat = data_flat @ w
 
-                    block.add_gradient(
-                        parameter,
-                        data_flat.reshape(X_gradient.data.shape[:-1] + (1,)),
-                        X_gradient.samples,
-                        X_gradient.components,
-                    )
-            blocks.append(block)
+        #             block.add_gradient(
+        #                 parameter,
+        #                 data_flat.reshape(X_gradient.data.shape[:-1] + (1,)),
+        #                 X_gradient.samples,
+        #                 X_gradient.components,
+        #             )
+        #     blocks.append(block)
 
-        self.alpha = tensor_map_to_dict(self.alpha)
-        return TensorMap(X.keys, blocks)
+        # self.alpha = tensor_map_to_dict(self.alpha)
+        return dot(X, self.coef_tensor)
 
     def score(self, X: TensorMap, y: TensorMap, parameter_key: str) -> List[float]:
         """Return the coefficient of determination of the prediction.
